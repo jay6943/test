174815f1f9d8a3884db9fc6e631d5bce3c8350dc
jay6943 <jkdavis@naver.com>
2023-07-22 21:09:32 +0900

    Samsung notebook

--- M2023.py
+++ M2023.py
@@ -1,7 +1,31 @@
+import cfg
 import numpy as np
 import matplotlib.pyplot as plt
 
-folder = '..//data/'
+def M230411():
+
+  a = np.linspace(0,1,361) * 2 * np.pi
+  x = np.cos(a)
+  y = np.sin(a)
+  l = 1.2
+
+  a1 = 180-45
+  x1 = np.cos(a[a1])
+  y1 = np.sin(a[a1])
+
+  plt.figure(figsize=(10,10))
+  plt.plot(x,y)
+  plt.plot([0,x1], [0,y1])
+  plt.plot([0,x1], [0,0])
+  plt.plot([x1,x1], [0,y1])
+  plt.xlim(-l,l)
+  plt.ylim(-l,l)
+  plt.xticks([-1,0,1])
+  plt.yticks([-1,0,1])
+  plt.grid()
+  plt.savefig(cfg.folder + 'M230411.png')
+
+  return plt
 
 def M220909():
 
@@ -10,14 +34,12 @@ def M220909():
   g = -3 * f - 1
   k = 1 / 2
 
-  _, ax = plt.subplots(1)
-
-  ax.plot(x,f)
-  ax.plot(x,g)
-  ax.plot([0,12],[k,k], '--')
-  ax.grid()
-
-  plt.savefig(folder + 'M220909.png')
+  plt.plot(x,f)
+  plt.plot(x,g)
+  plt.plot([0,12],[k,k], '--')
+  plt.xlim(x[0],x[-1])
+  plt.grid()
+  plt.savefig(cfg.folder + 'M220909.png')
 
   return plt
 
@@ -26,18 +48,16 @@ def M220607():
   x = np.linspace(0,1,361)
   f = -np.sin(2 * x * np.pi)
 
-  _, ax = plt.subplots(1)
-
-  ax.plot(x,f)
-  ax.plot([0.25,0.75,],[-1,1])
-  ax.set_xticks([0,0.25,0.5,0.75,1])
-  ax.set_xticklabels(['0','$\pi$/4','$\pi$/2','3$\pi$/4','$\pi$'])
-  ax.grid()
-
-  plt.savefig(folder + 'M220607.png')
+  plt.plot(x,f)
+  plt.plot([0.25,0.75,],[-1,1])
+  xl = ['0','$\pi$/4','$\pi$/2','3$\pi$/4','$\pi$']
+  plt.xticks([0,0.25,0.5,0.75,1], xl)
+  plt.xlim(x[0],x[-1])
+  plt.grid()
+  plt.savefig(cfg.folder + 'M220607.png')
 
   return plt
 
 if __name__ == '__main__':
 
-  M220607().show()
+  M230411().show()
--- T2023.py
+++ T2023.py
@@ -1,17 +1,8 @@
+import cfg
 import numpy as np
 import matplotlib.pyplot as plt
 
-folder = '..//data/'
-
-xl0 = ['0', '$\pi$/2', '$\pi$', '3$\pi$/2', '2$\pi$', '5$\pi$/2']
-xl1 = ['-5$\pi$/2', '-2$\pi$', '-3$\pi$/2', '-$\pi$', '-$\pi$/2'] + xl0
-xl2 = xl1.copy()
-xl3 = ['-9$\pi$/4', '-7$\pi$/4', '-5$\pi$/4', '-3$\pi$/4', '-$\pi$/4']
-xl4 = xl3 + ['$\pi$/4', '3$\pi$/4', '5$\pi$/4', '7$\pi$/4', '9$\pi$/4']
-
-for i in range(len(xl4)): xl2.insert(i*2+1, xl4[i])
-
-def T22_5():
+def T202205():
 
   x = np.linspace(0, 2, 1001)
 
@@ -21,44 +12,43 @@ def T22_5():
     f.append(t)
     y.append(np.tan(t * np.pi))
 
-  fig, ax = plt.subplots(1)
-  ax.plot([x[0], x[-1]], [0,0], '--')
-  for i in range(3): ax.plot(f[i], y[i], 'k-')
-  ax.plot(x, np.cos(x * np.pi), label=r'cos($\theta$)')
-  ax.plot(x, np.cos((x + 0.5) * np.pi), label=r'cos($\pi$/2+$\theta$)')
-  ax.set_xlabel(r'$\theta$')
-  ax.set(xlim=(0, 2), ylim=(-2, 2))
-  ax.set_xticks([0, 0.5, 1, 1.5, 2])
-  # ax.set_xticklabels(xl0)
-  ax.grid(linestyle=':')
+  plt.plot([x[0], x[-1]], [0,0], '--')
+  for i in range(3): plt.plot(f[i], y[i], 'k-')
+  plt.plot(x, np.cos(x * np.pi), label=r'cos($\theta$)')
+  plt.plot(x, np.cos((x + 0.5) * np.pi), label=r'cos($\pi$/2+$\theta$)')
+  plt.xlabel(r'$\theta$')
+  plt.xlim(0, 2)
+  plt.ylim(-2, 2)
+  xl = ['0','$\pi$/2','$\pi$','3$\pi$/2','2$\pi$']
+  plt.xticks([0,0.5,1,1.5,2], xl)
+  plt.grid(linestyle=':')
   plt.legend()
-  plt.savefig(folder + 'T22_5.png')
+  plt.savefig(cfg.folder + 'T202205.png')
 
   return plt
 
-def T22_9(period):
+def T202209():
 
   d, x, y = 0.01, [], []
 
   for i in range(7):
-    t = (np.linspace(d-0.5, 0.5-d, 101) + i - 2) / period
+    t = (np.linspace(d-0.5, 0.5-d, 101) + i - 2)
     x.append(t)
-    y.append(-np.tan(period * t * np.pi))
+    y.append(-np.tan(t * np.pi))
 
-  fig, ax = plt.subplots(1)
-  for i in range(7): ax.plot(x[i], y[i], 'k-')
-  ax.plot([1/2,1/2], [-4,4], '--')
-  ax.set_xlabel('x', fontdict={'family':'serif'}, style='italic')
-  ax.set(xlim=(-1/2, 1/2), ylim=(-4, 4))
-  xl = xl2[xl2.index('-$\pi$/2'):xl2.index('$\pi$/2')+1]
-  ax.set_xticks([(i * 0.5 - 1)/period for i in range(len(xl))])
-  ax.set_xticklabels(xl)
-  ax.grid(linestyle=':')
-  plt.savefig(folder + 'T22_9.png')
+  for i in range(7): plt.plot(x[i], y[i], 'k-')
+  plt.plot([1/2,1/2], [-4,4], '--')
+  plt.xlabel('x', fontdict={'family':'serif'}, style='italic')
+  plt.xlim(-1/2, 1/2)
+  plt.ylim(-4, 4)
+  xl = ['-$\pi$/2','-$\pi$/4','0','$\pi$/4','$\pi$/2']
+  plt.xticks([(i * 0.5 - 1) for i in range(len(xl))], xl)
+  plt.grid(linestyle=':')
+  plt.savefig(cfg.folder + 'T202209.png')
 
   return plt
 
-def T22_16():
+def T202216():
 
   d = 0.01
   x1 = np.linspace(d-2/3,12,101)
@@ -70,11 +60,11 @@ def T22_16():
   plt.plot(x2, f2, label='f2')
   plt.legend()
   plt.grid()
-  plt.savefig(folder + 'T22_16.png')
+  plt.savefig(cfg.folder + 'T202216.png')
 
   return plt
 
-def T22_19():
+def T202219():
 
   x = np.linspace(-2,4,1001)
   y = 2 * x**3 - 6 * x**2
@@ -83,11 +73,11 @@ def T22_19():
   plt.xlim(-2,4)
   plt.ylim(-10,2)
   plt.grid()
-  plt.savefig(folder + 'T22_16.png')
+  plt.savefig(cfg.folder + 'T202219.png')
 
   return plt
 
-def T22_21(arg, n):
+def T202221(arg, n):
 
   xmin, xmax = -4, 10
 
@@ -108,33 +98,12 @@ def T22_21(arg, n):
     plt.plot(x2, y2)
   plt.xlim([xmin,xmax])
   plt.grid()
-  plt.savefig(folder + 'T22_21_' + str(n) + '.png')
-
-  return plt
-
-def S230411():
-
-  a = np.linspace(0,1,361) * 2 * np.pi
-  x = np.cos(a)
-  y = np.sin(a)
-  l = 1.2
-
-  _, ax = plt.subplots(1, figsize=(10,10))
-
-  ax.plot(x,y)
-  ax.plot([0,np.cos(a[180-45])], [0,np.sin(a[180-45])])
-  ax.set_aspect('equal')
-  ax.set(xlim=(-l,l),ylim=(-l,l))
-  ax.set_xticks([-1,0,1])
-  ax.set_yticks([-1,0,1])
-  ax.grid()
-
-  plt.savefig(folder + 'S230411.png')
+  plt.savefig(cfg.folder + 'T202221-' + str(n) + '.png')
 
   return plt
 
 if __name__ == '__main__':
 
-  T22_5().show()
+  T202209().show()
 
-  # for n in range(10): T22_21(1, n).close()
+  # for n in range(10): T202221(1, n).close()

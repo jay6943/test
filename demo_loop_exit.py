d9319415e1b18ee51dc39c3f5671e4cf0e4eeb42
jay6943 <uetri@naver.com>
2023-11-24 13:36:22 +0900

    Dell Workstation

--- demo_loop_exit.py
+++ demo_loop_exit.py
@@ -1,12 +1,31 @@
 import keyboard
 import time
 
-j = 0
+def loop_exit():
+  try:
+    while 1:
+      for j in range(3):
+        print(j)
+        time.sleep(1)
+      print('done.')
+  except KeyboardInterrupt:
+    print('stop.')
 
-while 1:
-  if keyboard.is_pressed('esc'):
-    print('You pressed esc')
-    break
-  print(j)
-  j += 1
-  time.sleep(0.5)
+
+def loop_esc():
+  running = 1
+  while running:
+    for j in range(3):
+      print(j)
+      time.sleep(0.5)
+
+    for i in range(30):
+      if keyboard.is_pressed('esc'):
+        running = 0
+        break
+      print('-', end=' ')
+      time.sleep(0.1)
+    print('\b' * 100, end='')
+
+
+if __name__ == '__main__': loop_esc()
